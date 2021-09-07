666f49e00ca9d3775bc973e185e09eef43af0296
Brett Karlson <brettjkarlson@gmail.com>
2021-09-07 17:43:05 +0100

    books folder removed

--- routes/books/index.js
+++ routes/books/index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var books = require('../../controllers/books/index.js')
-console.log("look here")
-console.log(books)
 /* GET home page. */
 router.get('/books', books.books);
 
