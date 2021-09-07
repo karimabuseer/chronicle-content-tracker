82e1ed16524dc78574c4bcc2e1c600ec0309d28f
Sibel Eren <edaeren575@gmail.com>
2021-09-07 17:44:33 +0100

    users: sign up form + route

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/new', function(req,res,next) {
+  res.render("users/new");
+})
 module.exports = router;
