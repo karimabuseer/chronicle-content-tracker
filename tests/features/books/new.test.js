dd74f91c278dfa57c40f9e3ba5120b261e3918de
Sibel Eren <edaeren575@gmail.com>
2021-09-08 18:22:10 +0100

    books: end-to-end test for adding books

--- tests/features/books/new.test.js
+++ tests/features/books/new.test.js
@@ -6,4 +6,20 @@ describe('New book page', () => {
   it('should be titled "Add Book"', async () => {
     await expect(page.title()).resolves.toMatch('Add Book');
   });
+
+  it('should display new book in books list', async() => {
+    await page.type('#title', 'Harry Potter');
+    await page.type('#author', 'JK Rowling');
+    await page.click('#Add Book');
+
+    await page.goto('http://localhost:3000/books');
+
+    const title =  await page.$eval('.title', el => el.innerText);
+    const author =  await page.$eval('.author', el => el.innerText);
+
+    await expect(title).resolves.toMatch('Harry Potter');
+    await expect(author).resolves.toMatch('JK Rowling');
+  
+  })
 });
+
