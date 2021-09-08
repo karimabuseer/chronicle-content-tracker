8a8b64a20d83c77f27369abe14e08ba79828d7f4
karimabuseer <karimabuseer@gmail.com>
2021-09-08 14:42:00 +0100

    Added users routes

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use('/session', sessionRouter);
 app.use('/books', booksRouter);
 app.get('/books', booksRouter);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const { Router } = require('express'); //Can this line be deleted?
 var express = require('express');
 var router = express.Router();
 var users = require('../controllers/users.js')
@@ -5,7 +6,9 @@ var users = require('../controllers/users.js')
 
 /* GET users listing. */
 router.get('/', users.users);
-
 router.get('/new', users.users);
+router.post('/', users.users)
+router.put('/:id', users.users)
+router.delete('/:id', users.users)
 
 module.exports = router;
