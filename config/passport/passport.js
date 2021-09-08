1c3acfd84ab1d24c3186998b5006d578f51cfab8
karimabuseer <karimabuseer@gmail.com>
2021-09-08 23:04:39 +0100

    Changed passport local strategy to work with login

--- config/passport/passport.js
+++ config/passport/passport.js
@@ -4,13 +4,20 @@ module.exports = function(passport, user) {
   var User = user;
   var LocalStrategy = require('passport-local').Strategy;
 
-  passport.use('local-signup', new LocalStrategy(
- 
-    {
-      usernameField: 'email',
-      passwordField: 'password',
-      passReqToCallback: true
-    },
- 
-));
+  passport.use(new LocalStrategy(
+    function(username, password, done) {
+      User.findOne({ username: username }, function (err, user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      });
+    }
+  ));
+    
+    
 };
