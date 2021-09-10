a4611f0a34c481f960592c589a2bb4f11ae78c0b
karimabuseer <karimabuseer@gmail.com>
2021-09-10 14:30:42 +0100

    Removed comments from session.js

--- routes/session.js
+++ routes/session.js
@@ -12,15 +12,5 @@ router.get('/new', session.login);
 router.post('/', 
   passport.authenticate('local', { successRedirect: '/', failureRedirect: '/users/new' } 
     ));
-/* might need this later - working user being passed into cookies, but login fail goes to passport's
-default 401 error page 
-function (req, res) {
-  if (req.user) {
-    console.log('the session is' + req.session.passport.user)
-    res.redirect('/users/' + req.user.user_name)
-  } else {
-    return res.redirect('/session/new')
-  }
-} */
 module.exports = router;
 
