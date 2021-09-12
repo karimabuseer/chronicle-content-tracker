b64a99a47bbf876c56fbcfc373d00a4547be37a6
karimabuseer <karimabuseer@gmail.com>
2021-09-12 21:19:24 +0100

    Making changes to books index

--- tests/features/books/index.test.js
+++ tests/features/books/index.test.js
@@ -1,9 +1,22 @@
 describe('Book list page', () => {
   beforeAll(async () => {
+    await page.goto('http://localhost:3000/sessions/new');
+    await expect(page).toFillForm('form[name="sign-up-form"]', {
+      username: 'username',
+      email: 'email@email.com',
+      password: 'password'
+    })
+    await page.goto('http://localhost:3000/sessions/new');
+    await expect(page).toFillForm('form[name="sign-up-form"]', {
+      username: 'username',
+      email: 'email@email.com',
+      password: 'password'
+    })
     await page.goto('http://localhost:3000/books');
   });
 
   it('should be titled "Books List"', async () => {
-    await expect(page.title()).resolves.toMatch('Book List');
+    const title =  await page.$eval('.title', el => el.innerText);
+    expect(title).toMatch('Book List');
   });
 });
