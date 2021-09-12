8ef263d00458977b65910281f91187b16ffdf9fa
Else Meijerink <p.e.meijerink@gmail.com>
2021-09-12 17:05:13 +0100

    Clear form on submit to prevent double entry into database

--- controllers/users.js
+++ controllers/users.js
@@ -7,16 +7,13 @@ module.exports = {
   },
 
   add (req, res) {
-  console.log('we are in add')
   return user
     .create({
       user_name: req.body.username,
       email: req.body.email,
       encrypted_password: req.body.password
     })
-    .then((user) => res.render('users/new', { 
-      title: 'Sign Up'
-    }))
+    .then(() => res.redirect("/"))
     .catch((error) => res.status(400).send(error));
   },
 };
