553476fcc34002570f00349d122f327948a9cd84
William Young <wadyoung@outlook.com>
2021-09-14 15:50:08 +0100

    Added status update functionality

--- routes/usersBooks.js
+++ routes/usersBooks.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 var addUserBook = require('../controllers/userBooks/new')
 var deleteUserBook = require('../controllers/userBooks/delete')
+var updateUserBook = require('../controllers/userBooks/update')
 
 router.get('/:id', addUserBook.add);
 
 router.post('/delete', deleteUserBook.delete);
+
+router.post('/update', updateUserBook.update);
 
 module.exports = router;
