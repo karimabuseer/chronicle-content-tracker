8e085c4b3a7d939e4208654bc839e26fea8815b1
Else Meijerink <p.e.meijerink@gmail.com>
2021-09-14 18:02:47 +0100

    Removed redundant code added correct render

--- controllers/books/index.js
+++ controllers/books/index.js
@@ -1,23 +1,5 @@
 const usersBook = require('../../models').users_book;
 
 exports.books = function(req, res, next) {
-
-try { 
-  return usersBook
-    .findAll( {
-      where: { user_id: req.session.passport.user },
-      include: 'book'
-     })
-    .then((userBooks) => {
-      const userBookArray = [];
-      userBooks.forEach((userBook) => {
-        userBookArray.push(userBook.dataValues)
-      })
-      return userBookArray
-    })
-    .then( (userBookArray) => res.render( "books/index", {userBookList: userBookArray, title: "Book List"}))
-  }
-  catch {
-    res.redirect('/session/new')
-  }
+  res.render("books/index");
 };
