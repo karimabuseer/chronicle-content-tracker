5140a7eb7b56317a520b2a1c2347445c4e570278
Else Meijerink <p.e.meijerink@gmail.com>
2021-09-14 21:45:12 +0100

    Updates new user tests

--- tests/features/users/new.test.js
+++ tests/features/users/new.test.js
@@ -4,20 +4,17 @@ describe('Sign Up Page', () => {
   });
 
   it('should have a sign up form', async() => {
-    await expect(page.title()).resolves.toMatch('Sign Up');
+    const title =  await page.$eval('h1', el => el.innerText);
+    expect(title).toMatch('Sign Up');
   })
 
-  it('sign up then login should direct you to profile page', async() => {
+  it('sign up should direct you to home page', async() => {
     await expect(page).toFillForm('form[name="sign-up-form"]', {
       username: 'username',
       email: 'email@email.com',
       password: 'password'
     })
     await page.click('#sign-up-button');
-    await expect(page.url()).toMatch('http://localhost:3000/users')
-  })
-
-  it('sign up then login should direct you to profile page', async() => {
-    
+    await expect(page.url()).toMatch('http://localhost:3000')
   })
 });
