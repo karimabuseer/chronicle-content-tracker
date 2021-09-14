5c46a84d7d2c63a8e0ddc0032f162ea511831c6e
Else Meijerink <p.e.meijerink@gmail.com>
2021-09-14 21:41:01 +0100

    Added login so that basic /books test can pass

--- tests/features/books/index.test.js
+++ tests/features/books/index.test.js
@@ -1,4 +1,11 @@
 describe('Book list page', () => {
+
+  // fix when crud / authentication is complete
+  it('should be redirect you to the log in page if you are not logged in', async () => {
+    await page.goto('http://localhost:3000/books');
+    await expect(page.url()).toMatch('http://localhost:3000/session/new');
+  });
+
   beforeAll(async () => {
     await page.goto('http://localhost:3000/users/new');
     await expect(page).toFillForm('form[name="sign-up-form"]', {
@@ -6,18 +13,21 @@ describe('Book list page', () => {
       email: 'email@email.com',
       password: 'password'
     })
+    await page.click('#sign-up-button');
+
+    await page.goto('http://localhost:3000/session/new');
+    await expect(page).toFillForm('form[name="log-in-form"]', {
+      email: 'email@example.com',
+      password: 'Example'
+    })
+    await page.click('#log-in-button');
     await page.goto('http://localhost:3000/books');
   });
 
   it('should be titled "Books Finder"', async () => {
-    const title =  await page.$eval('.title', el => el.innerText);
+    await expect(page.url()).toMatch('http://localhost:3000/books');
+    await expect(page.title()).resolves.toMatch("Chronicle")
+    const title =  await page.$eval('h1', el => el.innerText);
     expect(title).toMatch('Book Finder');
   });
-
-  it('should be redirect you to the log in page if you are not logged in', async () => {
-    await expect(page.url()).toMatch('http://localhost:3000/session/new');
-  });
-
-  
-
 });
