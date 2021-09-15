a6a48ea7bbbd7cc6c6b11c1df6888830952b0460
Else Meijerink <p.e.meijerink@gmail.com>
2021-09-15 12:27:56 +0100

    Implemented dynamic navbar for book search and book list

--- controllers/books/index.js
+++ controllers/books/index.js
@@ -1,5 +1,8 @@
 const usersBook = require('../../models').users_book;
 
 exports.books = function(req, res, next) {
-  res.render("books/index");
+  res.render('books/index', { 
+    title: 'Book Search',
+    user: req.session.passport.user
+  })
 };
