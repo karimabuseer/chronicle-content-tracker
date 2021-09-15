a0240b124b62e778657111011d4e59035e032982
karimabuseer <karimabuseer@gmail.com>
2021-09-16 00:16:13 +0100

    Added additional routes for updating user books

--- routes/usersMovies.js
+++ routes/usersMovies.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var addUserMovie = require('../controllers/userMovies/new')
-
+var deleteUserMovie = require('../controllers/userBooks/delete')
+var updateUserMovie = require('../controllers/userBooks/update')
+var updateRatingUserMovie = require('../controllers/userBooks/updateRating')
 
 
 router.get('/:id', addUserMovie.add);
+
+router.post('/delete', deleteUserMovie.delete);
+
+router.post('/update', updateUserMovie.updateStatus);
+
+router.post('/updateRating', updateRatingUserMovie.updateRating);
 
 
 module.exports = router;
