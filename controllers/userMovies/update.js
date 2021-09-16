89fdb237b4786bd20f82e335937f0106e12487f6
karimabuseer <karimabuseer@gmail.com>
2021-09-16 08:51:51 +0100

    Created an update rating method in userMovies controller

--- controllers/userMovies/update.js
+++ controllers/userMovies/update.js
@@ -11,15 +11,16 @@ module.exports = {
     .then(() => console.log(req.body.status), res.redirect(`/users/${req.session.passport.user}/movies`))
     .catch((error) => res.status(400).send(error));
     },
+    
     updateRating (req, res) {
-      return usersBook
+      return usersMovie
       .update(
         { rating: req.body.rating },
         { where: {
           user_id: req.session.passport.user,
-          id: req.body.book_id }
+          id: req.body.movie_id }
         })
-      .then(() => res.redirect(`/users/${req.session.passport.user}/books`))
+      .then(() => res.redirect(`/users/${req.session.passport.user}/movies`))
       .catch((error) => res.status(400).send(error));
       }
 };
