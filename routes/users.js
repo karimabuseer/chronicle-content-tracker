e9005fc93c132f763f2f0fba9ab1d7c7b0a15666
karimabuseer <karimabuseer@gmail.com>
2021-09-16 17:00:32 +0100

    Removed delete user controller from user route that is crashing production

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,7 @@ var router = express.Router();
 var users = require('../controllers/users.js')
 const usersBook = require('../controllers/userBooks/index')
 const movieList = require('../controllers/userMovies/index.js')
-const delete_user = require('../controllers/delete_user.js')
-const delete_page = require('../controllers/delete_page.js')
-const user_password = require('../controllers/user_password.js')
+
 
 /* GET users listing. */
 router.get('/new', users.sign_up);
