111f1d438e2bb739aad68d0df3771ae1113fde57
karimabuseer <karimabuseer@gmail.com>
2021-09-16 19:05:22 +0100

    Redirect added to userBppks

--- controllers/userBooks/index.js
+++ controllers/userBooks/index.js
@@ -1,18 +1,23 @@
 const usersBook = require('../../models').users_book;
 
 exports.books = function(req, res, next) {
-  return usersBook
-    .findAll( {
-      where: { user_id: req.session.passport.user },
-      include: 'book',
-      order: [['createdAt', 'DESC']] 
-     })
-    .then((userBooks) => {
-      const userBookArray = [];
-      userBooks.forEach((userBook) => {
-        userBookArray.push(userBook.dataValues)
+  try {
+    return usersBook
+      .findAll( {
+        where: { user_id: req.session.passport.user },
+        include: 'book',
+        order: [['createdAt', 'DESC']] 
       })
-      return userBookArray
-    })
-    .then( (userBookArray) => res.render( "userBooks/index", {user: req.session.passport.user, userBookList: userBookArray, title: "Book List"}));
+      .then((userBooks) => {
+        const userBookArray = [];
+        userBooks.forEach((userBook) => {
+          userBookArray.push(userBook.dataValues)
+        })
+        return userBookArray
+      })
+      .then( (userBookArray) => res.render( "userBooks/index", {user: req.session.passport.user, userBookList: userBookArray, title: "Book List"}));
+  }
+  catch { 
+    res.redirect('/session/new')
+  }
 };
--- controllers/userMovies/index.js
+++ controllers/userMovies/index.js
@@ -4,21 +4,21 @@ const movie = require('../../models').movie;
 
 exports.movies = function(req, res, next) {
   try {
-  return usersMovie
-    .findAll( {
-      where: { user_id: req.session.passport.user },
-      include: 'movie',
-      order: [['createdAt', 'DESC']] 
-     })
-    .then((userMovies) => { 
-      const userMovieArray = [];
-      userMovies.forEach((userMovie) => {
-        userMovieArray.push(userMovie.dataValues)
+    return usersMovie
+      .findAll( {
+        where: { user_id: req.session.passport.user },
+        include: 'movie',
+        order: [['createdAt', 'DESC']] 
       })
-      return userMovieArray
-    }
-    )
-    .then( (userMovieArray) => res.render( "userMovies/index", {user: req.session.passport.user, userMovieList: userMovieArray, title: "Movie List"}));
+      .then((userMovies) => { 
+        const userMovieArray = [];
+        userMovies.forEach((userMovie) => {
+          userMovieArray.push(userMovie.dataValues)
+        })
+        return userMovieArray
+      }
+      )
+      .then( (userMovieArray) => res.render( "userMovies/index", {user: req.session.passport.user, userMovieList: userMovieArray, title: "Movie List"}));
   } catch {
     res.redirect('/session/new')
   }
