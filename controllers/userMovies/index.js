64262a1061822cc64d7fd6a7220dcb9ea061e362
karimabuseer <karimabuseer@gmail.com>
2021-09-16 19:04:42 +0100

    Redirect added to userMovies

--- controllers/userMovies/index.js
+++ controllers/userMovies/index.js
@@ -3,6 +3,7 @@ const movie = require('../../models').movie;
 
 
 exports.movies = function(req, res, next) {
+  try {
   return usersMovie
     .findAll( {
       where: { user_id: req.session.passport.user },
@@ -18,4 +19,7 @@ exports.movies = function(req, res, next) {
     }
     )
     .then( (userMovieArray) => res.render( "userMovies/index", {user: req.session.passport.user, userMovieList: userMovieArray, title: "Movie List"}));
+  } catch {
+    res.redirect('/session/new')
+  }
 };
